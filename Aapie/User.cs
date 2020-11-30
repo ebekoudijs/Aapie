9e3a9b910777de7dc6469e66065c706190ed9112
PhilipLenssen <philiplenssen@gmail.com>
2020-11-30 10:11:40 +0100

    User class

---
+++ Aapie/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string UName, string PWord)
+        {
+            Username = UName;
+            Password = PWord;
+        }
+    }
+}
