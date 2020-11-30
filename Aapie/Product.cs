a32dbb637ee38ef0565854c1434d9c27f84af192
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-30 10:58:30 +0100

    Product.cs en Table.cs toegevoegd

---
+++ Aapie/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public class Product
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public double AlcPercent { get; set; }
+        public int Stock { get; set; }
+        public double AmountCl { get; set; }
+        public string Description { get; set; }
+        public Product(double price, string name, double alcPercent, int stock, double amountCl, string description) {
+            Price = price;
+            Name = name;
+            AlcPercent = alcPercent;
+            Stock = stock;
+            AmountCl = amountCl;
+            Description = description;
+        }
+    }
+}
