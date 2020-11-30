e09ddb97b3f071955ee4ecc2e3bf147f16430995
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-30 10:37:41 +0100

    Order.cs toegevoegd

---
+++ Aapie/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public class Order
+    {
+        public double Totalprice() {
+            double totalprice = 12;
+            return totalprice;
+        }
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        public Table Table { get; set; }
+        public List<Product> Products { get; set; }
+        public string Status { get; set; }
+
+        public Order(DateTime date, string message, Table table, List<Product> products, string status) {
+            Date = date;
+            Message = message;
+            Table = table;
+            Products = products;
+            Status = status;
+        }
+    }   
+}
