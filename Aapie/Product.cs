6b7cbcad85bb7b07b94ef037700151afb2053116
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-12-16 09:18:20 +0100

    ID added to product

--- Aapie/Product.cs
+++ Aapie/Product.cs
@@ -7,13 +7,15 @@ namespace Aapie
 {
     public class Product
     {
+        public int Id { get; set; }
         public double Price { get; set; }
         public string Name { get; set; }
         public double AlcPercent { get; set; }
         public int Stock { get; set; }
         public double AmountCl { get; set; }
         public string Description { get; set; }
-        public Product(string name, double price, double alcPercent, int stock, double amountCl, string description) {
+        public Product(int id, string name, double price, double alcPercent, int stock, double amountCl, string description) {
+            Id = id;
             Price = price;
             Name = name;
             AlcPercent = alcPercent;
