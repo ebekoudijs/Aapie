819e5265a55ab0873e130d6ee0544c236be714dc
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-12-16 09:14:20 +0100

    User class changed to look more like the database

--- Aapie/User.cs
+++ Aapie/User.cs
@@ -7,15 +7,33 @@ namespace Aapie
 {
     public class User
     {
+        public string UserId { get; set; }
         public string Username { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        public User(string UName, string PWord, string PNumber)
+       
+
+        
+        
+        
+        public User(string UID, string username, string gender, int age, string email, string password)
         {
-            Username = UName;
-            Password = PWord;
-            PhoneNumber = PNumber;
+            UserId = UID;
+            Username = username;
+            Gender = gender;
+            Age = age;
+            Email = email;
+            Password = password;
+            Gender = "Man";
+            Age = 25;
+            Email = "Test";
         }
-        public User() { }
+        public User() {
+            Gender = "Man";
+            Age = 25;
+            Email = "Test";
+        }
     }
 }
