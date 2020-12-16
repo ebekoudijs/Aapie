083b4c2f417db7ba7dbc45d701d8ecb17e2842cf
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-12-16 09:18:54 +0100

    Orderline added

---
+++ Aapie/OrderLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public class OrderLine
+    {
+        public OrderLine(Product product)
+        {
+            Product Product = product;
+        }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }   
+}
