d3e0249b7161dc3f97d5198d5db7fe7862131fc8
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-12-16 09:17:39 +0100

    Mysql settings changed, adduser now saves more information, replaced incremental id with guid, removed getuser, added getdrinks, addorder and changed CheckCredentials

--- Aapie/Database.cs
+++ Aapie/Database.cs
@@ -6,12 +6,10 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 //gemaakt met deze tut https://www.codeproject.com/articles/43438/connect-c-to-mysql
 namespace Aapie
 {
-    public class Database 
+    public class Database
     {
         private MySqlConnection connection;
         private string server;
@@ -22,10 +20,10 @@ namespace Aapie
             Initialize();
         }
         private void Initialize() {
-            server = "localhost";
-            database = "cohroboticstest";
-            uid = "root";
-            password = "";
+            server = "192.168.68.117";
+            database = "database busn";
+            uid = "Root";
+            password = "Root";
             string connectionString;
             connectionString = "SERVER=" + server + ";" + "DATABASE=" +
             database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
@@ -40,7 +38,7 @@ namespace Aapie
                 {
                     await connection.OpenAsync();
                 }
-                
+
                 return connection;
             }
             catch (MySqlException exception)
@@ -48,7 +46,7 @@ namespace Aapie
                 switch (exception.Number)
                 {
                     case 0:
-                        Console.WriteLine("Server is offline");
+                        Console.WriteLine("Datavase server is offline");
                         break;
 
                     case 1045:
@@ -72,23 +70,28 @@ namespace Aapie
                 return false;
             }
         }
-        //Nu staan er alleen nog extreem algemene functies in (zoals de functie insert) deze moeten nog vervangen worden voor functies zoals adduser, dit is te doen door delen van de query aanpasbaar te maken als parameters in de functie
-        public async Task AddUser(User user) {
+        public async Task<User> AddUser(User user) {
+            Guid guid = Guid.NewGuid();
+            string guidString = guid.ToString();
+            user.UserId = guidString;
             MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
             cmd.Connection = await OpenConnection();
-            cmd.CommandText = "INSERT INTO user(username, password, phonenumber) VALUES(@username, @password, @phonenumber)";
+            cmd.CommandText = "INSERT INTO klant(UserID, Naam, Geslacht, Leeftijd, Email, Wachtwoord) VALUES(@userID, @username, @geslacht, @leeftijd, @email, @password)";
             cmd.Prepare();
+            cmd.Parameters.AddWithValue("@userID", user.UserId);
             cmd.Parameters.AddWithValue("@username", user.Username);
+            cmd.Parameters.AddWithValue("@geslacht", user.Gender);
+            cmd.Parameters.AddWithValue("@leeftijd", user.Age);
+            cmd.Parameters.AddWithValue("@email", user.Email);
             cmd.Parameters.AddWithValue("@password", user.Password);
-            cmd.Parameters.AddWithValue("@phonenumber", user.PhoneNumber);
             cmd.ExecuteNonQuery();
             await CloseConnection();
+            user.Password = null;
+            return user;
         }
         public async Task RemoveUser(int id)
         {
             MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
             cmd.Connection = await OpenConnection();
             cmd.CommandText = "DELETE FROM user WHERE id = @id";
             cmd.Prepare();
@@ -96,37 +99,12 @@ namespace Aapie
             cmd.ExecuteNonQuery();
             await CloseConnection();
         }
-        public async Task<User> GetUser(int id)
-        {
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
-            cmd.Connection = await OpenConnection();
-            cmd.CommandText = "SELECT * FROM user WHERE userID = @id";
-            cmd.Prepare();
-            cmd.Parameters.AddWithValue("@id", id);
-            MySqlDataReader myReader = (await cmd.ExecuteReaderAsync() as MySqlDataReader);
-            bool result = await myReader.ReadAsync();
 
-            if (result)
-            {
-                string username = myReader.GetString("username");
-                string phonenumber = myReader.GetString("phonenumber");
-                User user = new User(username, null, phonenumber);
-                await CloseConnection();
-                return user;
-            }
-            else 
-            {
-                await CloseConnection();
-                return null;
-            }
-        }
-        public async Task<bool> CheckCredentials(string username, string password)
+        public async Task<User> CheckCredentials(string username, string password)
         {
             MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
             cmd.Connection = await OpenConnection();
-            cmd.CommandText = "SELECT * FROM user WHERE username = @username AND password = @password";
+            cmd.CommandText = "SELECT * FROM klant WHERE Naam = @username AND Wachtwoord = @password";
             cmd.Prepare();
             cmd.Parameters.AddWithValue("@username", username);
             cmd.Parameters.AddWithValue("@password", password);
@@ -134,43 +112,76 @@ namespace Aapie
             bool result = await myReader.ReadAsync();
             if (result)
             {
-                string dbusername = myReader.GetString("username");
-                string dbphonenumber = myReader.GetString("phonenumber");
+                string dbid = myReader.GetString("userID");
+                string dbusername = myReader.GetString("Naam");
+                string dbGender = myReader.GetString("Geslacht");
+                int dbAge = myReader.GetInt32("Leeftijd");
+                string dbEmail = myReader.GetString("Email");
+
                 await myReader.CloseAsync();
-                User loggedUser = new User(dbusername, null, dbphonenumber);
+                User loggedUser = new User(dbid, dbusername, dbGender, dbAge, dbEmail, null);
                 await CloseConnection();
-                return true;
+                return loggedUser;
             }
             else
             {
                 await CloseConnection();
-                return false;
+                return null;
             }
         }
         public async Task<List<Product>> GetDrinks() {
             List<Product> Productlist = new List<Product>();
 
-
             MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
             cmd.Connection = await OpenConnection();
-            cmd.CommandText = "SELECT * FROM products";
+            cmd.CommandText = "SELECT * FROM artikel";
             cmd.Prepare();
             MySqlDataReader myReader = (await cmd.ExecuteReaderAsync() as MySqlDataReader);
-            while (await myReader.ReadAsync() )
+            while (await myReader.ReadAsync())
             {
-                string name = myReader.GetString("name");
-                string Description = myReader.GetString("description");
-                double price = myReader.GetDouble("price");
-                double AmountCl = myReader.GetDouble("amountcl");
-                int Stock = myReader.GetInt32("stock");
-                double AlcPercent = myReader.GetDouble("alcpercent");
-                Product drink = new Product(name, price, AlcPercent, Stock, AmountCl, Description);
+                int id = myReader.GetInt32("DRANKid");
+                string name = myReader.GetString("Naam");
+                double price = myReader.GetDouble("Prijs");
+                string Description = myReader.GetString("Beschrijving");
+                double AmountCl = myReader.GetDouble("HoeveelheidCL");
+                int Stock = myReader.GetInt32("Stock");
+                double AlcPercent = myReader.GetDouble("AlcPercent");
+                Product drink = new Product(id, name, price, AlcPercent, Stock, AmountCl, Description);
                 Productlist.Add(drink);
             }
             await myReader.CloseAsync();
             await CloseConnection();
             return Productlist;
+        }
+        public async Task<Order> addOrder(Order order, User user) {
+            Guid guid = Guid.NewGuid();
+            string guidString = guid.ToString();
+            foreach (var orderline in order.OrderLines)
+            {
+                await addOrderLine(orderline, guidString);
+            }
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = await OpenConnection();
+            cmd.CommandText = "INSERT INTO bestelling(OrderID, UserID, MdwID, TableID, RobotID) VALUES(@OrderID, @UserID, @MdwID, @TableID, @RobotID)";
+            cmd.Prepare();
+            cmd.Parameters.AddWithValue("@OrderID", guidString);
+            cmd.Parameters.AddWithValue("@UserID", user.UserId);
+            cmd.Parameters.AddWithValue("@MdwID", 5);
+            cmd.Parameters.AddWithValue("@TableID", 5);
+            cmd.Parameters.AddWithValue("@RobotID", 5);
+            return order;
+        }
+        public async Task<OrderLine> addOrderLine(OrderLine orderLine, string guidString) {
+            Order order = new Order();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = await OpenConnection();
+            cmd.CommandText = "INSERT INTO bestelregel(OrderID, DrankID, Aantal) VALUES(@OrderID, @DrankID, @Quantity)";
+            cmd.Prepare();
+            cmd.Parameters.AddWithValue("@OrderID", guidString);
+            cmd.Parameters.AddWithValue("@DrankID", orderLine.Product.Id);
+            cmd.Parameters.AddWithValue("@Quantity", orderLine.Quantity);
+
+            return null;
         }
     }
 }
