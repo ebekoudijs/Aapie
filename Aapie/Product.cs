7ceeb915f56e761dbe41ecac5575913f61911ffc
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2021-01-05 10:22:20 +0100

    id veranderd naar productid

--- Aapie/Product.cs
+++ Aapie/Product.cs
@@ -7,15 +7,15 @@ namespace Aapie
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int ProductId { get; set; }
         public double Price { get; set; }
         public string Name { get; set; }
         public double AlcPercent { get; set; }
         public int Stock { get; set; }
         public double AmountCl { get; set; }
         public string Description { get; set; }
-        public Product(int id, string name, double price, double alcPercent, int stock, double amountCl, string description) {
-            Id = id;
+        public Product(int productId, string name, double price, double alcPercent, int stock, double amountCl, string description) {
+            ProductId = productId;
             Price = price;
             Name = name;
             AlcPercent = alcPercent;
