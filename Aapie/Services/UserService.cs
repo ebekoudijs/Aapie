88847ab7ea9bbd72c1307f45091121647f004fe8
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2021-01-06 10:07:04 +0100

    Userservice aangemaakt

---
+++ Aapie/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Aapie
+{
+    public class UserService : IUserService
+    {
+        private readonly Database _database;
+        public UserService(Database database) {
+            _database = database;
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            return await _database.AddUser(user);
+        }
+
+        public Task<User> GetUser(string id)
+        {
+            return _database.GetUser(id);
+        }
+        public async Task RemoveUser(string id)
+        {
+            await _database.RemoveUser(id);
+        }
+        public async Task<User> ChangeUser(User user)
+        {
+            return new User();
+        }
+    }
+}
