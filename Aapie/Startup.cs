91e326f849efeaee8f8a23686abc0f2dca9e510e
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2021-01-06 10:01:18 +0100

    Services toegevoegd

--- Aapie/Startup.cs
+++ Aapie/Startup.cs
@@ -24,7 +24,9 @@ namespace Aapie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(new Database());
+            services.AddSingleton <IUserService, UserService>();
+            services.AddSingleton <IOrderService, OrderService>();
+            services.AddSingleton<Database>();
             services.AddControllers();
         }
 
