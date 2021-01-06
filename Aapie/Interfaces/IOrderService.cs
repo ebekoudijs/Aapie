96f381b02b0c86a4991ae2e7fee894b55c42ab10
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2021-01-06 10:06:46 +0100

    Orderservice aangemaakt

---
+++ Aapie/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public interface IOrderService
+    {
+        Task<Order> AddOrder(Order order, User user);
+        Task UpdateDeliveryDate(string orderId);
+    }
+}
---
+++ Aapie/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aapie
+{
+    public class OrderService : IOrderService
+    {
+        private readonly Database _database;
+        public OrderService(Database database) {
+            _database = database;
+        }
+        public async Task<Order> AddOrder(Order order, User user)
+        {
+            return await _database.addOrder(order, user);
+        }
+        public async Task UpdateDeliveryDate(string orderId) {
+            await _database.UpdateDeliveryDate(orderId);
+        }
+    }
+}
